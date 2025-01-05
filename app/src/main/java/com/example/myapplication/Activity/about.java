3d33dd8c555b3ac28cbce133ad70145adfa0a634
DridiMayssen <dridimaysen301@gmail.com>
2025-01-05 02:13:28 +0100

    commit

---
+++ app/src/main/java/com/example/myapplication/Activity/about.java
@@ -0,0 +1,36 @@
+package com.example.myapplication.Activity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ImageButton;
+
+import androidx.activity.EdgeToEdge;
+import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.graphics.Insets;
+import androidx.core.view.ViewCompat;
+import androidx.core.view.WindowInsetsCompat;
+
+import com.example.myapplication.R;
+
+public class about extends AppCompatActivity {
+    ImageButton btnBack;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        EdgeToEdge.enable(this);
+        setContentView(R.layout.activity_about);
+        btnBack=findViewById(R.id.backk);
+        btnBack.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplicationContext(), MainActivity.class);
+                startActivity(intent);
+                finish();
+            }
+        });
+
+    }
+}
